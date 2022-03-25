5b9df81c1487b30545da8b3c4e99fbe1d2834b8b
keipy27 <kevinandris27@gmail.com>
2022-03-26 12:12:23 +1300

    my fourth commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Route, Routes } from 'react-router-dom';
+import './app.css';
+
+import CreateBook from './components/CreateBook';
+import ShowBookList from './components/ShowBookList';
+import ShowBookDetails from './components/ShowBookDetails';
+import UpdateBookInfo from './components/UpdateBookInfo';
+
+
+function App() {
+  return (
+    <div className='App'>
+      <Routes>
+        <Route path="/" element={<ShowBookList/>} />
+        <Route path="/create-book" element={<CreateBook/>} />
+        <Route path="/Book-details" element={<ShowBookDetails/>} />
+        <Route path="/update-info" element={<UpdateBookInfo/>} />
+
+      </Routes>
+    </div>
+  )
+}
+
+export default App;
